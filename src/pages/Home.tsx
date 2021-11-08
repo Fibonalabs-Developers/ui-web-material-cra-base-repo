761e1593616fc5b34c5d4a513ddb06b926b823c3
Sehal Sein <sehal.sein@fibonalabs.com>
2021-11-08 11:03:02 +0530

    Init Commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,36 @@
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useDummyList } from 'src/api/demo';
+
+const HomePage = () => {
+  const { data } = useDummyList();
+
+  return (
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <Typography variant="h1" component="h2">
+        Home Page
+      </Typography>
+
+      <Button color="primary" variant="contained">
+        Test Button
+      </Button>
+      <ul>
+        {data?.map(({ title, id }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default HomePage;
